909deb77ea730bf31d0b435f92596d10a14a0f39
Adam Boyher <aboyher@gmail.com>
2018-08-30 08:18:02 -0500

    Added library(car)

--- app.R
+++ app.R
@@ -15,6 +15,7 @@ library(RSQLite)
 library(FactoMineR)
 library(factoextra)
 library(ggridges)
+library(car)
 
 ui <- dashboardPage(skin="black", title="Metabolomics",
                     dashboardHeader(
